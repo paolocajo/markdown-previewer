e6101a2be884efe9e069507a1309c5cf2a24e163
Paolo Martín Cajo Flores <paolocajo09@gmail.com>
2023-09-10 14:19:32 -0500

    Adding delete markdown button

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 
 function App() {
-  const [markdown, setMarkdown] = useState("# Markdown Preview");
+  const defaultMarkdown = "# Markdown Preview";
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
   return (
     <main>
       <div className="markdown">
-        <textarea
-          className="input"
-          id="editor"
-          placeholder="Write your Markdown here..."
-          value={markdown}
-          onChange={(e) => setMarkdown(e.target.value)}
-        ></textarea>
+        <div className="editor-container">
+          <textarea
+            className="input"
+            id="editor"
+            placeholder="Write your Markdown here..."
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          ></textarea>
+          <button
+            className="btn-delete"
+            onClick={() => {
+              setMarkdown(defaultMarkdown);
+            }}
+          >
+            Delete
+          </button>
+        </div>
         <article id="preview" className="result">
           <ReactMarkdown remarkPlugins={[gfm]}>{markdown}</ReactMarkdown>
         </article>
